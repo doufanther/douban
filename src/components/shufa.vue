5f8814a887ec88981232d579e202c4eeb4086885
doufanther <zhengshufa@126.com>
2017-08-01 15:31:09 +0800

    0

---
+++ src/components/shufa.vue
@@ -0,0 +1,26 @@
+<template>
+  	<div class="shufa">
+  			88
+  	</div>
+</template>
+
+<script>
+	
+	
+  export default {
+    data() {
+      return {
+        
+      }
+    },
+   	created(){
+   		
+   	},
+    methods:{
+    	
+	}
+  }
+</script>
+<style scoped>
+	
+</style>
